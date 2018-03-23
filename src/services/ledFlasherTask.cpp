8c9342749bd621fcb8e323420b7084e62e7a99ee
bootchk <bootch@nc.rr.com>
2018-03-23 15:48:02 -0400

    Led flasher use one-shot event, reenabled. New PinTask API.

--- src/services/ledFlasherTask.cpp
+++ src/services/ledFlasherTask.cpp
@@ -26,20 +26,21 @@ void LEDFlasherTask::init(uint32_t pin ) {
 	assert(Counter::isTicking());
 
 	// PinTask must be init
-	PinTask::configureTaskOnPinToToggleInitiallyHigh( pin );
+	PinTask::configureSunkPinTasks( pin );
 	PinTask::enableTask();
 
 	// Now, we are only users of PPI
 	/*
 	 * Connect signal from EventTimer to task
 	 */
-	uint32_t* taskAddress = PinTask::getTaskRegisterAddress();
+	uint32_t* taskAddress = PinTask::getSunkOffTaskRegisterAddress();
 	uint32_t* eventAddress = EventTimer::getEventRegisterAddress();
 
 	RTTLogger::log(" eventTimer address: ");
 	RTTLogger::log((uint32_t) eventAddress);
 
-	EventToTaskSignal::connect(eventAddress,taskAddress);
+	//EventToTaskSignal::connect(eventAddress,taskAddress);
+	EventToTaskSignal::connectOneShot(eventAddress,taskAddress);
 }
 
 
@@ -54,10 +55,13 @@ void LEDFlasherTask::flashLEDByAmount(unsigned int amount) {
 	// Return if already flashing.
 	// TODO is there any way to do this?
 
-	// toggle LED on
+	// LED on
 	// RTTLogger::log("LED on\n");
-	PinTask::startTask();
+	PinTask::startSunkOnTask();
 
 	// start timer that will generate event connected via PPI to task which will toggle off
 	EventTimer::start(LEDFlasher::amountInTicks(amount));
+
+	// Re enable one-shot connection to off task
+	EventToTaskSignal::enableOneShot();
 }
